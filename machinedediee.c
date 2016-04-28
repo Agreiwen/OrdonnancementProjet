5e12e4cbb74803c0f6d0588a253e00da6de1d800
Thibault Delavelle <delavell2u@fst-ats-319-l06.fst.site.univ-lorraine.fr>
2016-04-28 10:39:12 +0200

    Modifications sur les taches (plus de machines, les familles suffisent)

--- machinedediee.c
+++ machinedediee.c
@@ -1,4 +1,4 @@
-/* Projet */
+/* Projet Ordonancement : Machines dédiées */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,7 +8,6 @@ struct tache {
   int pi;
   int ri;
   int ti;
-  int machine;
   int famille;
 };
 
@@ -33,6 +32,9 @@ llist ajouterEnTete(llist liste, tache tache1){
 }
 
 
+
+
+
 void afficherListeTaches(llist taches){
 
   element *tmp = taches;
@@ -41,10 +43,21 @@ void afficherListeTaches(llist taches){
     printf("pi : %i",tmp->t.pi);
     printf("| ri : %i",tmp->t.ri);
     printf("| ti : %i",tmp->t.ti);
-    printf("| machine : %i",tmp->t.machine);
     printf("| famille : %i\n",tmp->t.famille);
     tmp = tmp->next;
   }
+}
+
+
+
+int algoGenetique(int heuristique, llist taches, int nbIterations){
+  return 20;
+
+}
+
+
+int algoBranchAndBound(int heuristique, llist taches){
+  return 10;
 }
 
 
@@ -55,13 +68,21 @@ int main(int argc, char **argv){
   struct element *ma_liste3 = NULL;
   */
 
-  tache t1 = { .pi = 5, .ri = 5, .ti = 0, .machine = 1, .famille = 7 };
-  tache t2 = { .pi = 3, .ri = 0, .ti = 0, .machine = 3, .famille = 1 };
-  tache t3 = { .pi = 7, .ri = 2, .ti = 0, .machine = 2, .famille = 5 };
-  tache t4 = { .pi = 1, .ri = 0, .ti = 0, .machine = 2, .famille = 4 };
-  tache t5 = { .pi = 2, .ri = 3, .ti = 0, .machine = 3, .famille = 3 };
-  tache t6 = { .pi = 4, .ri = 0, .ti = 0, .machine = 1, .famille = 6 };
+  tache t1 = { .pi = 5, .ri = 5, .ti = 0, .famille = 7 };
+  tache t2 = { .pi = 3, .ri = 0, .ti = 0, .famille = 1 };
+  tache t3 = { .pi = 7, .ri = 2, .ti = 0, .famille = 5 };
+  tache t4 = { .pi = 1, .ri = 0, .ti = 0, .famille = 4 };
+  tache t5 = { .pi = 2, .ri = 3, .ti = 0, .famille = 3 };
+  tache t6 = { .pi = 4, .ri = 0, .ti = 0, .famille = 6 };
   
+  /* famille : 1 sur la machine 1 uniquement 
+               2 sur la 2
+               3 sur la 3 
+               4 sur 1 et 2 
+               5 sur 2 et 3 
+               6 sur 1 et 3 
+               7 sur les 3  
+  */
   
   llist taches = NULL;
   taches = ajouterEnTete(taches, t1);
@@ -73,15 +94,48 @@ int main(int argc, char **argv){
 
   afficherListeTaches(taches);
 
-
+  int algo = 1; // 1= genetique 2= b&b
+  int heuristique = 1; 
+  int nbIterations = 50; 
+  
+ 
+  int resultat = 0;
+  switch(algo){
+  case 1:
+    resultat = algoGenetique(heuristique, taches, nbIterations);
+    break;
+  case 2:
+    resultat = algoBranchAndBound(heuristique, taches);
+    break;
+  default:
+    printf("l'algo n'exsite pas\n");
+    break;
+  }
+   
+ printf("RESULTAT de l'algorithme : %i\n",resultat);
 
   // 1. definir heuristique 
   // choix de l'utilisateur (tapez 1 pour h1, 2 pour h2, etc.)
   //    --> un entier pour l'heuristique et un switch pour savoir quelle action réaliser dans l'algo
   
   // 2. appeler l'algo avec h en paramètre et la liste de taches 
-  //    --> une fonction par algo : int algoGenetique(int h,llist taches) et int algoBranchAndBound(int h, llist tahces) 
+  //    --> une fonction par algo : int algoGenetique(int h,llist taches, int nbIterations) et 
+  //                                int algoBranchAndBound(int h, llist taches) 
   //        qui renvoie une durée totale d'execution des taches
+
+
+
+
+
+  /* Fonctionement interface terminal 
+     1. On demande le nombre de taches
+     2. On demande les taches une par une
+     3. On demande l'algo à appliquer
+     4. On demande l'heuristique à utiliser
+     5. Si c'est génétique : on demande le nombre d'itérations
+     6. On lance l'algo 
+     7. On affiche le résultat
+  */
 
   return 0;
 }  
