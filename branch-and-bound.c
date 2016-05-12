671c0b1eda012dbfe6cea4c29d25c41266245903
Aurélien <aurel.andre@yahoo.fr>
2016-05-12 09:36:11 +0200

    Ajout du fichier pour branch and bound

---
+++ branch-and-bound.c
@@ -0,0 +1,30 @@
+/* Projet Ordonnancement : Machines dédiées - branch and bound */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <string.h>
+#include <time.h>
+
+/* Definition structure de tache */
+typedef struct noeud noeud;
+typedef struct listnoeud listnoeud;
+
+struct noeud
+{
+    int val;
+    listnoeud *enfants;
+};
+
+struct listnoeud
+{
+    noeud *node;
+    listnoeud *next;
+};
+
+int main(int argc, char **argv){
+    printf("****** Projet d'ordonnancement ******\n\n");
+    return 0;
+}
